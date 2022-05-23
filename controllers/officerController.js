5229bd8b1aa115b31761008e8dbca0f7d4dfe957
Kalana99 <kalanarajika99@gmail.com>
2022-05-24 01:17:58 +0530

    add new vehicle/ get all vehicles completed

--- controllers/officerController.js
+++ controllers/officerController.js
@@ -5,6 +5,7 @@ const SuperUser = require('../models/superuser');
 const Officer = require('../models/officer');
 const Owner = require('../models/owner');
 const Notification = require('../models/notification');
+const Vehicle = require('../models/vehicle');
 
 const auth = require('../middleware/auth');
 const encHandler = require('../middleware/encryptionHandler');
@@ -52,12 +53,97 @@ const login_post = async (req, res) => {
     }
 }
 
-//add new vehicle
+// add new vehicle
+const add_vehicle = async (req, res) => {
 
-//update vehicle
+    let data = req.body;
+    let ownerNIC = req.body.ownerNIC;
 
-//download files
+    const owner = await Owner.findOne({nic: ownerNIC})
+
+    if(owner != null){
+
+        const vehicle = new Vehicle(data);
+
+        vehicle.save((err) => {
+            
+            if(err){
+
+                let errmsg = "Registration number already exists!";
+
+                res.json({
+                    status: 'error',
+                    error: errmsg,
+                });
+            }
+            else{
+                // send notification to user with a generated liscense
+                res.json({
+                    status: 'ok',
+                    vehicle: vehicle
+                });
+            }
+        })
+    }
+    else{
+
+        res.json({
+            status: 'error',
+            error: 'Invalid owner!',
+        });
+    }
+}
+
+//get all vehicles
+const get_vehicles = async (req, res) => {
+
+    try {
+        
+        let vehicles = await Vehicle.find();
+        let owners = await Owner.find();
+
+        if(vehicles.length > 0){
+
+            let result = [];
+
+            vehicles.forEach((vehicle) => {
+
+                let nic = vehicle.ownerNIC;
+
+                for (let i = 0; i < owners.length; i++) {
+
+                    let owner = owners[i];
+
+                    if(owner.nic == nic){
+
+                        result.push({
+                            vehicle,
+                            owner
+                        });
+                        continue;
+                    }
+                }
+            });
+
+            res.json({
+                status: 'ok',
+                result: result
+            });
+        }
+        else{
+            res.json({
+                status: 'empty',
+                result: []
+            })
+        }
+    }
+    catch (err) {
+        console.log(err);
+    }
+}
 
 module.exports = {
     login_post,
+    add_vehicle,
+    get_vehicles,
 }
--- routes/officerRoutes.js
+++ routes/officerRoutes.js
@@ -7,4 +7,10 @@ const router = express.Router();
 //login
 router.post('/login', officerController.login_post);
 
+//add new vehicle
+router.post('/addVehicle', auth.requireAuth, officerController.add_vehicle);
+
+//get all vehicles
+router.get('/getAllVehicles', auth.requireAuth, officerController.get_vehicles);
+
 module.exports = router;
