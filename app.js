7554805164412f8183e6a09e6e341274168e3c5b
Kalana99 <kalanarajika99@gmail.com>
2022-06-03 16:32:31 +0530

    reload default route

--- app.js
+++ app.js
@@ -24,6 +24,9 @@ app.use(express.urlencoded({ extended: true }));
 // set static file path for productioin build
 if (process.env.NODE_ENV === 'production'){
 	app.use(express.static('client/build'));
+    app.get("/*", function(req, res) {
+        res.sendFile(path.join(__dirname, "./client/build/index.html"));
+    });
 }
 
 // connect to mongodb and listen
@@ -41,4 +44,3 @@ app.use((req, res, next) => {
 app.use('/superuser', superuserRoutes);
 app.use('/officer', officerRoutes);
 app.use('/owner', ownerRoutes);
-app.get('/*', (req, res) => res.send('Index Page'));
