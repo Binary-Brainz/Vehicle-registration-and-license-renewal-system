323c44ba24b5f43bc3b7dcc1e80186d5f31f48cf
Dakshina Ranmal <76484102+dakshina99@users.noreply.github.com>
2022-06-04 02:52:53 +0530

    done

--- client/src/components/Owner/OwnVehiclesComponent.js
+++ client/src/components/Owner/OwnVehiclesComponent.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { Button, Card, Modal } from 'react-bootstrap';
+import { Button, Card, Form, Modal } from 'react-bootstrap';
 import ViewVehicleComponent from './ViewVehicleComponent';
 import { useSelector, useDispatch } from 'react-redux';
+import ReactCrop from 'react-image-crop';
 import { gotId, gotNic } from '../userSlice';
+import { FileUpload } from 'primereact/fileupload';
 
 const axios = require('axios').default;
 
@@ -10,7 +12,7 @@ const OwnVehicles = () => {
 
     const token = sessionStorage.getItem('owner_token');
 
-    if(!token){
+    if (!token) {
         sessionStorage.clear();
         document.location = '/';
     }
@@ -24,6 +26,7 @@ const OwnVehicles = () => {
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [defaultVehicle, setDefaultVehicle] = useState("/assets/images/vehicle2.gif");
     const [ownVehicles, setOwnVehicles] = useState([]);
+    const [vId, setVId] = useState("");
 
     useEffect(() => {
 
@@ -39,15 +42,15 @@ const OwnVehicles = () => {
                 let vehicles = response.data.vehicles;
                 let user_data = response.data.user;
 
-                if(status === 'ok'){
+                if (status === 'ok') {
                     setOwnVehicles(vehicles);
                     setUser(user_data);
                 }
-                else if(status === 'auth-error'){
+                else if (status === 'auth-error') {
                     sessionStorage.clear();
                     document.location = '/';
                 }
-                else{
+                else {
                     console.log(response.error);
                 }
             })
@@ -67,6 +70,61 @@ const OwnVehicles = () => {
         };
     }
 
+    const [isImageModal, setIsImageModal] = useState(false);
+
+    const toggleImageModal = () => setIsImageModal(!isImageModal);
+    const changeImage = () => {
+        toggleImageModal();
+    }
+
+    const [srcImg, setSrcImg] = useState(null);
+    const [image, setImage] = useState(null);
+    const [crop, setCrop] = useState({ aspect: 16 / 9 });
+    const [result, setResult] = useState(null);
+
+    const handleImage = async (event) => {
+        setSrcImg(URL.createObjectURL(event.target.files[0]));
+        console.log(event.target.files[0]);
+    };
+
+    const getCroppedImg = async () => {
+        try {
+            const canvas = document.createElement("canvas");
+            const scaleX = image.naturalWidth / image.width;
+            const scaleY = image.naturalHeight / image.height;
+            canvas.width = crop.width;
+            canvas.height = crop.height;
+            const ctx = canvas.getContext("2d");
+            ctx.drawImage(
+                image,
+                crop.x * scaleX,
+                crop.y * scaleY,
+                crop.width * scaleX,
+                crop.height * scaleY,
+                0,
+                0,
+                crop.width,
+                crop.height
+            );
+
+            const base64Image = canvas.toDataURL("image/jpeg", 1);
+            setResult(base64Image);
+            console.log(result);
+        } catch (e) {
+            console.log("crop the image");
+        }
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        console.log(result);
+    }
+
+    const onUpload = () => {
+
+        console.log("done")
+    }
+
     var vehicle = ownVehicles.filter(function (el) {
         return el._id === id;
     })[0];
@@ -77,8 +135,8 @@ const OwnVehicles = () => {
 
                 {ownVehicles.map((vehicle) => (
 
-                    <Card key={vehicle._id} bg="light" border="light" className='m-3 shadow' style={{ width: '18rem', "paddingLeft": "0px", "paddingRight": "0px" }}>
-                        <Card.Img variant="top" src={(vehicle.image) ? vehicle.image : defaultVehicle} />
+                    <Card key={vehicle._id} bg="light" border="light" className='m-3 shadow' style={{ height: "24rem", width: '18rem', "paddingLeft": "0px", "paddingRight": "0px" }}>
+                        <Card.Img height="200rem" className='crdhv' onMouseOut={() => setVId("")} onMouseOver={() => setVId(vehicle._id)} onClick={changeImage} variant="top" src={(vehicle.image) ? vehicle.image : ((vId === vehicle._id) ? "/assets/images/edit.jpg" : defaultVehicle)} />
                         <Card.Body>
                             <Card.Title>{vehicle.regNo}</Card.Title>
                             <Card.Subtitle>{vehicle.type} ({vehicle.model})</Card.Subtitle>
@@ -93,6 +151,22 @@ const OwnVehicles = () => {
 
             <Modal show={isModalOpen} onHide={toggleModal}>
                 <ViewVehicleComponent vehicle={vehicle} />
+            </Modal>
+
+            <Modal show={isImageModal} onHide={toggleImageModal}>
+                <Modal.Header closeButton>Change Image</Modal.Header>
+                <Modal.Body >
+                    <FileUpload name="demo[]" url="https://binary-brainz-bucket.s3.us-west-2.amazonaws.com/uploads" onUpload={onUpload} accept="image/*" maxFileSize={1000000}
+                        emptyTemplate={<p className="m-0">Drag and drop image to here to upload.</p>} />
+                    <div>
+
+                        {result && (
+                            <div>
+                                <img src={result} alt="cropped image" />
+                            </div>
+                        )}
+                    </div>
+                </Modal.Body>
             </Modal>
         </div>
 
---
+++ client/src/components/Owner/new.js
@@ -0,0 +1,8 @@
+
+
+    return (
+        <Container className="container" fluid="md">
+            <h5 className="header">React Image Crop</h5>
+            
+        </Container>
+    );
